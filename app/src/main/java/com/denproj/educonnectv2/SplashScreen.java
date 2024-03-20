52bd328deb99316782678cd9fc546872cd955473
jeyse <jcfaustino110@gmail.com>
2024-03-21 00:22:29 +0800

    gege

--- app/src/main/java/com/denproj/educonnectv2/SplashScreen.java
+++ app/src/main/java/com/denproj/educonnectv2/SplashScreen.java
@@ -18,7 +18,7 @@ import com.denproj.educonnectv2.databinding.ActivitySplashScreenBinding;
 public class SplashScreen extends AppCompatActivity {
 
     ActivitySplashScreenBinding binding;
-    Animation in_anim, out_anim;
+    Animation in_anim, out_anim, left_anim, right_anim, up_anim, down_anim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +27,18 @@ public class SplashScreen extends AppCompatActivity {
 
         in_anim = AnimationUtils.loadAnimation(this, R.anim.scale_in_animation);
         out_anim = AnimationUtils.loadAnimation(this, R.anim.scale_out_animation);
+        left_anim = AnimationUtils.loadAnimation(this, R.anim.left_animation);
+        right_anim = AnimationUtils.loadAnimation(this, R.anim.right_animation);
+        up_anim = AnimationUtils.loadAnimation(this, R.anim.up_animation);
+        down_anim = AnimationUtils.loadAnimation(this, R.anim.down_animation);
 
-        binding.logo.startAnimation(in_anim);
+        binding.leftTitle.startAnimation(left_anim);
+        binding.rightTitle.startAnimation(right_anim);
+        binding.topViews.startAnimation(down_anim);
+        binding.bottomViews.startAnimation(up_anim);
+
+
+  /*      binding.logo.startAnimation(in_anim);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -39,7 +49,7 @@ public class SplashScreen extends AppCompatActivity {
 
             }
         },1800);
-
+*/
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -49,7 +59,7 @@ public class SplashScreen extends AppCompatActivity {
                 finish();
 
             }
-        },2300);
+        },1500);
 
 
 
