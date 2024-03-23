8f436ca03245459722ef03e7c9be6337014253c3
denver6000 <denverballesteros01@gmail.com>
2024-03-23 10:23:33 +0800

    Added Logout feature on ProfileFragment.java

--- app/src/main/java/com/denproj/educonnectv2/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/denproj/educonnectv2/ui/profile/ProfileFragment.java
@@ -1,14 +1,21 @@
 package com.denproj.educonnectv2.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.denproj.educonnectv2.MainActivity;
 import com.denproj.educonnectv2.R;
+import com.denproj.educonnectv2.databinding.FragmentProfileBinding;
+import com.denproj.educonnectv2.util.UITask;
+import com.denproj.educonnectv2.viewModel.DashboardViewModel;
 
 
 public class ProfileFragment extends Fragment {
@@ -16,9 +23,25 @@ public class ProfileFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        FragmentProfileBinding binding = FragmentProfileBinding.inflate(inflater);
+
+        DashboardViewModel viewModel = new ViewModelProvider(requireActivity()).get(DashboardViewModel.class);
+        binding.floatingLogOut.setOnClickListener(view -> viewModel.logOut(new UITask<Void>() {
+            @Override
+            public void onSuccess(Void result) {
+                Intent intent = new Intent(requireActivity(), MainActivity.class);
+                startActivity(intent);
+                requireActivity().finish();
+            }
+
+            @Override
+            public void onFail(String message) {
+
+            }
+        }));
+
+        return binding.getRoot();
     }
 }
