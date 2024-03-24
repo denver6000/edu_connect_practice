32d5e311cd5205758b1b2b939126adf76206f5a9
denver6000 <denverballesteros01@gmail.com>
2024-03-25 06:34:31 +0800

    Added add group fragment

--- app/src/main/java/com/denproj/educonnectv2/ui/dashboard/group/AddGroupFragment.java
+++ app/src/main/java/com/denproj/educonnectv2/ui/dashboard/group/AddGroupFragment.java
@@ -11,16 +11,15 @@ import android.view.ViewGroup;
 
 import com.denproj.educonnectv2.R;
 import com.denproj.educonnectv2.databinding.FragmentAddNewsBinding;
-import com.denproj.educonnectv2.databinding.FragmentGroupBinding;
 
-public class GroupFragment extends Fragment {
+
+public class AddGroupFragment extends Fragment {
 
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        FragmentGroupBinding binding = FragmentGroupBinding.inflate(inflater);
-
+        FragmentAddNewsBinding binding = FragmentAddNewsBinding.inflate(inflater);
         return binding.getRoot();
     }
 }
